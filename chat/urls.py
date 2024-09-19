2cd733b3da91f51a49b153e30618f0ce64c066c8
YERCKEN <yercken30@gmail.com>
2024-09-18 21:03:00 -0500

    + VIsta ROOM Ya en esta versión por lo menos puedes crear la sala y puedes escribir pero aún no se envía nada por que no existe un consumidor

--- chat/urls.py
+++ chat/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+                                    # Index
     path("", views.index, name="index"),
+                                                # Room
+    path("<str:room_name>/", views.room, name="room"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # INDEX ======================================================
 def index(request):
     return render(request, "index.html")
+
+# ROOM =======================================================
+def room(request, room_name):
+    return render(request, "room.html", {"room_name": room_name})
