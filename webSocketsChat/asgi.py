d26659d9f67b342dc7c2ec40fcbdfd23ae741a1e
YERCKEN <yercken30@gmail.com>
2024-09-18 21:57:12 -0500

    Customer Listo

--- webSocketsChat/asgi.py
+++ webSocketsChat/asgi.py
@@ -1,16 +1,12 @@
-"""
-ASGI config for webSocketsChat project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+
+# !importamos el chat rrouting
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
@@ -18,6 +14,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
+    
     "http": django_asgi_app,
+    # !Wbsocket
+    "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        ),
     # Just HTTP for now. (We can add other protocols later.)
 })
