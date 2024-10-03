03407884b8ee7a035283eed54537c5a44e4c5fe4
des <des@gmail.com>
2024-10-02 21:38:29 -0500

    upload new files

---
+++ chat/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def home(request):
+  return render(request, 'home.html') 
+
+
+def room(request):
+  return render(request, 'room.html') 
+
+
+def guestroom(request):
+  return render(request, 'guestroom.html')
+
+
+def echoroom(request):
+  return render(request, 'echoroom.html') 
