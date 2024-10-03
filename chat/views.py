6ed938a5fa455d192ab1366231215008e14016b8
des <des@gmail.com>
2024-10-02 21:29:00 -0500

    reset

--- chat/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# INDEX ======================================================
-def index(request):
-    return render(request, "index.html")
-
-# ROOM =======================================================
-def room(request, room_name):
-    return render(request, "room.html", {"room_name": room_name})
